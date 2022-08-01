872e2ed75d46a2cab55a1174197caffe4d5e74c6
EmilioAcostaG <103851807+EmilioAcostaG@users.noreply.github.com>
2022-07-31 21:24:35 -0400

    Added layout of page, time to add JS

---
+++ Assets/script.js
@@ -0,0 +1,58 @@
+
+// var timeEl = document.querySelector(".time");
+// var secondsLeft = 60;
+
+// // set time interval
+// function setTime() {
+//   var timerInterval = setInterval(function() {
+//     secondsLeft --;
+//     timeEl.textContent = secondsLeft;
+//   }, 1000);
+// }
+
+var quizData = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    a: "strings",
+    b: "booleans",
+    c: "alerts",
+    d: "numbers",
+    correct: "c"
+  },
+
+  {
+    question: "The condition in an if / else statement is enclosed within _____.",
+    a: "quotes",
+    b: "curly brackets",
+    c: "parantheses",
+    d: "square brackets",
+    correct: "c"
+  },
+
+  {
+    question: "Arrays in JavaScript can be used to store _____.",
+    a: "numbers and strings",
+    b: "other arrays",
+    c: "booleans",
+    d: "all of the above",
+    correct: "d"
+  },
+
+  {
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    a: "commas",
+    b: "curly brackets",
+    c: "quotes",
+    d: "parantheses",
+    correct: "c",
+  },
+
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    a: "JavaScript",
+    b: "terminal/bash",
+    c: "for loops",
+    d: "console log",
+    correct: "d"
+  }
+]
