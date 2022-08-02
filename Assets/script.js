e7f5dfb83e75e5ed9b76632838946199ac67532e
EmilioAcostaG <103851807+EmilioAcostaG@users.noreply.github.com>
2022-08-02 00:00:06 -0400

    updated README

--- Assets/script.js
+++ Assets/script.js
@@ -10,23 +10,65 @@
 //   }, 1000);
 // }
 
+var quizMe = document.querySelector(".quiz-button");
+var header = document.querySelector(".page-header")
+var questionData = document.querySelector("#question-container")
+var questionEl = document.querySelector("#question")
+var answerOptions = document.querySelector(".answer")
+
+var currentQuestion = {}
+var acceptinAnswers = true
+var score = 0
+var questionCounter = 0
+
+
+quizMe.addEventListener("click", startQuiz);
+
+
+function startQuiz() {
+  quizMe.classList.add("hide")
+  header.classList.add("hide")
+  questionData.classList.remove("hide")
+  availableQuestions = [...quizData]
+  getNewQuestion()
+
+}
+
+function getNewQuestion() {
+  for (var i=0; i < availableQuestions.length; i++) {
+    var questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    questionEl.innerText = currentQuestion.question;
+  }
+
+  answer.forEach(answer); {
+    var number = answer.dataset["number"]
+    answerOptions.innerText = currentQuestion['answer' + number]
+  }
+
+ 
+
+} 
+
+
+
 var quizData = [
   {
-    question: "Commonly used data types DO NOT include:",
+    question: "Commonly used data types DO NOT include:", 
     a: "strings",
     b: "booleans",
     c: "alerts",
     d: "numbers",
-    correct: "c"
+    correctAnswer: "c",
   },
 
   {
-    question: "The condition in an if / else statement is enclosed within _____.",
+    question: "The condition in an if / else statement is enclosed within _____.", 
     a: "quotes",
     b: "curly brackets",
     c: "parantheses",
     d: "square brackets",
-    correct: "c"
+    correct: "c",
   },
 
   {
@@ -35,7 +77,7 @@ var quizData = [
     b: "other arrays",
     c: "booleans",
     d: "all of the above",
-    correct: "d"
+    correctAnswer: "d",
   },
 
   {
@@ -44,7 +86,7 @@ var quizData = [
     b: "curly brackets",
     c: "quotes",
     d: "parantheses",
-    correct: "c",
+    correctAnswer: "c",
   },
 
   {
@@ -53,6 +95,11 @@ var quizData = [
     b: "terminal/bash",
     c: "for loops",
     d: "console log",
-    correct: "d"
+    correctAnswer: "d", 
   }
 ]
+
+
+
+
+quizMe.addEventListener("click", startQuiz);
---
+++ README.md
@@ -0,0 +1,6 @@
+
+In this first screenshot, you will see the home page.
+![Screenshots of Quiz, First Page](./Assets/images/Quiz%20Screenshot1.png)
+
+This next screenshot shows what the question looks like. Still need to link questions to html.
+![Screenshot of question](./Assets/images/Quiz%20Screenshot2.png)
